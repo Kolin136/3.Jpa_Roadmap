aa153e632499aa5e39ae6322ffb65a3eded708fb
Kolin136 <jsowb136@gmail.com>
2024-03-21 16:38:36 +0900

    "섹션3-2~4"

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -2,6 +2,7 @@ package study.datajpa.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,18 +13,44 @@ import study.datajpa.entity.Member;
 
 @SpringBootTest
 @Transactional
-@Rollback(false)
 public class MemberRepositoryTest {
+
   @Autowired
   MemberRepository memberRepository;
   @Test
   public void testMember() {
     Member member = new Member("memberA");
     Member savedMember = memberRepository.save(member);
-    Member findMember = memberRepository.findById(savedMember.getId()).get();
+    Member findMember =
+        memberRepository.findById(savedMember.getId()).get();
     Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
 
     Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
     Assertions.assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성 보장
   }
+  @Test
+  public void basicCRUD() {
+    Member member1 = new Member("member1");
+    Member member2 = new Member("member2");
+    memberRepository.save(member1);
+    memberRepository.save(member2);
+    //단건 조회 검증
+    Member findMember1 = memberRepository.findById(member1.getId()).get();
+    Member findMember2 = memberRepository.findById(member2.getId()).get();
+    assertEquals(findMember1,member1);
+    assertEquals(findMember2,member2);
+
+    //리스트 조회 검증
+    List<Member> all = memberRepository.findAll();
+    assertEquals(all.size(),2);
+    //카운트 검증
+    long count = memberRepository.count();
+    assertEquals(count,2);
+    //삭제 검증
+    memberRepository.delete(member1);
+    memberRepository.delete(member2);
+    long deletedCount = memberRepository.count();
+    assertEquals(deletedCount,0);
+  }
+
 }
