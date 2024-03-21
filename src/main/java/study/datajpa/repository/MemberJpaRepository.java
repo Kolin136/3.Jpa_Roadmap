2ed6459ac11d95b710a93c9cc2b23c231f59dab6
Kolin136 <jsowb136@gmail.com>
2024-03-21 16:02:16 +0900

    "섹션3-1"

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -2,17 +2,39 @@ package study.datajpa.repository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberJpaRepository {
-  @PersistenceContext
-  private EntityManager em;
+
+  private final EntityManager em;
   public Member save(Member member) {
     em.persist(member);
     return member;
   }
+
+  public void delete(Member member){
+    em.remove(member);
+  }
+
+  public List<Member> findAll(){
+    return em.createQuery("select m from Member m", Member.class).getResultList();
+  }
+
+  public Optional<Member> findById(Long id){
+    Member member = em.find(Member.class, id);
+    return Optional.ofNullable(member);
+  }
+
+  public Long count(){
+    return em.createQuery("select count(m) from Member m",Long.class).getSingleResult();
+  }
+
   public Member find(Long id) {
     return em.find(Member.class, id);
   }
--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -2,14 +2,17 @@ package study.datajpa.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
 @SpringBootTest
+@Transactional
 class MemberJpaRepositoryTest {
 
   @Autowired
@@ -25,6 +28,38 @@ class MemberJpaRepositoryTest {
     Member findMember = memberJpaRepository.find(saveMember.getId());
     //then
     assertEquals(findMember.getId(),member.getId());
+  }
+  
+  @Test
+  public void basicCRUD() throws Exception {
+    //given
+    Member member1 = new Member("member1");
+    Member member2 = new Member("member2");
+    //when
+    memberJpaRepository.save(member1);
+    memberJpaRepository.save(member2);
+
+    Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+    Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+    List<Member> all = memberJpaRepository.findAll();
+
+    Long count = memberJpaRepository.count();
+    //then
+    // 단건 조회
+    assertEquals(findMember1,member1);
+    assertEquals(findMember2,member2);
+    // 리스트 조회
+    assertEquals(all.size(),2);
+    // 카운트 검증
+    assertEquals(count,2);
+    // 삭제 검증
+    memberJpaRepository.delete(member1);
+    memberJpaRepository.delete(member2);
+    Long deleteCount = memberJpaRepository.count();
+    assertEquals(deleteCount,0);
+
+
   }
   
   
